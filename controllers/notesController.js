ebb1d852d73312972a5f36304ff01d3b40d5fdc5
Ishan Singh <nameste380@gmail.com>
2024-02-13 10:57:12 -0800

    addingMoreAPI

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -7,7 +7,7 @@ module.exports = {
             const chapterPdf = `/upload/chapterNotes/${req.file.filename}`
             chapterNotesData.chapterPdf = chapterPdf
             await chapterNotesData.save()
-            res.status(201).send({
+            res.status(202).send({
                 success: true,
                 message: "Chapter notes is successfully uploaded!"
             })
@@ -15,8 +15,91 @@ module.exports = {
             res.status(500).send({
                 success: false,
                 message: "Server Error",
-                error: error
+                error: error.message
             })
         }
-    }
+    },
+
+    deleteChapterNotes: async (req, res) => {
+        try {
+            const chapterNotesId = req.params.id
+            const chapterNotesData = await notesSchema.findByIdAndDelete(chapterNotesId)
+            if (!chapterNotesData) {
+                return res.status(401).send({
+                    success: false,
+                    message: "Chapter Id is incorrect , try again"
+                })
+            }
+            res.status(200).send({
+                success: true,
+                message: "Chapter is deleted successfully!"
+            })
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Server Error",
+                error: error.message
+            })
+        }
+    },
+
+    updateChapterNotes: async (req, res) => {
+        try {
+            const chapterId = req.params.id;
+            const chapterNotesData = await notesSchema.findByIdAndUpdate(chapterId, req.body);
+            if (!chapterNotesData) {
+                return res.status(401).send({
+                    success: false,
+                    message: "Chapter Id is incorrect , try again"
+                })
+            }
+            res.status(200).send({
+                success: true,
+                message: 'Chapter Notes is updated successfully!.'
+            })
+        }
+        catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Server Error",
+                error: error.message
+            })
+        }
+    },
+
+    viewAllChapterNotes: async (req, res) => { // Need to also show comments 
+        try {
+            const allChapterNotesData = await notesSchema.find()
+            res.status(200).send({
+                success: true,
+                message: "All chapters found",
+                chaptersData: allChapterNotesData,
+            })
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Server Error",
+                error: error.message
+            })
+        }
+    },
+
+    addChapterNoteLike: async (req, res) => {
+        try {
+            const chapterNoteId = req.params.id
+            const chapterNoteData = await notesSchema.findById(chapterNoteId)
+            chapterNoteData.chapterLikes++
+            await chapterNoteData.save()
+            res.status(200).send({
+                success: true,
+                message: "You Like chapter!"
+            })
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: "Server Error",
+                error: error.message
+            })
+        }
+    },
 }
--- routes/chapterNotesRoute.js
+++ routes/chapterNotesRoute.js
@@ -6,5 +6,9 @@ const { uploadPDF } = require('../middlewares/chapterNotesStorage')
 const chapterNotesRouter = express.Router()
 
 chapterNotesRouter.post('/addChapterNotes', uploadPDF.single('chapterPdf'), notesController.addChapterNotes)
+chapterNotesRouter.delete('/deleteChapterNotes/:id', notesController.deleteChapterNotes)
+chapterNotesRouter.patch('/updateChapterNotes/:id', notesController.updateChapterNotes)
+chapterNotesRouter.get('/viewAllChapterNotes', notesController.viewAllChapterNotes)
+chapterNotesRouter.get('/addChapterNoteLike/:id', notesController.addChapterNoteLike)
 
 module.exports = chapterNotesRouter
