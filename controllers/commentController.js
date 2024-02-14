c36f208d2439c874af69212736ce3558515fcf1b
Ishan Singh <nameste380@gmail.com>
2024-02-14 06:03:22 -0800

    addedCommentLogger

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,5 @@
 const commentSchema = require('../models/commentModel')
+const commentLogger = require('../utils/commentLogger/commentLogger')
 
 module.exports = {
     addComment: async (req, res) => {
@@ -7,11 +8,14 @@ module.exports = {
             const commentData = new commentSchema(req.body)
             commentData.chapterNotesId = chapterId
             await commentData.save()
+            commentLogger.log('info', "Comment added!")
             res.status(202).send({
                 success: true,
                 message: "Comment added!"
             })
         } catch (error) {
+            commentLogger.log('error', "Internal Server Error!")
+            commentLogger.log('error', `Error: ${error.message}`)
             res.status(500).send({
                 success: false,
                 message: "Server Error",
@@ -25,16 +29,20 @@ module.exports = {
             const commentId = req.params.id
             const commentData = await commentSchema.findByIdAndDelete(commentId)
             if (!commentData) {
+                commentLogger.log('error', "Comment ID is incorrect")
                 return res.status(400).send({
                     success: false,
                     message: "Comment ID is incorrect!"
                 })
             }
+            commentLogger.log('info', "Comment deleted!")
             res.status(202).send({
                 success: true,
                 message: "Comment deleted!"
             })
         } catch (error) {
+            commentLogger.log('error', "Internal Server Error!")
+            commentLogger.log('error', `Error: ${error.message}`)
             res.status(500).send({
                 success: false,
                 message: "Server Error",
